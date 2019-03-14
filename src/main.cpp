f5bd50474cb196eb36964669c1d1695838e9d34a
Leo <leoberto.soares@gmail.com>
2019-03-14 09:11:23 -0300

    Update project

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <string>
+
+extern "C" {
+#include <libavdevice/avdevice.h>
+#include <libavfilter/avfilter.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+}
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -7,30 +16,71 @@
 using namespace std;
 using namespace cv;
 
-void ReadingCamera() {
-    Mat frame;
-    VideoCapture *pVCapture = nullptr;
-    pVCapture = new VideoCapture(1);
 
-    if(!pVCapture->isOpened()) {
-        std::cout << "Could open video stream" << std::endl;
-        exit(0);
-    }
+typedef struct {
+    AVFormatContext *pFmtCtx    = nullptr;
+    AVCodecContext  *pCodecCtx  = nullptr;
+    AVCodec         *pCodec     = nullptr;
+    AVStream        *pStream    = nullptr;
+    AVFrame         *pFrame     = nullptr;
+    AVPacket        *pPacket    = nullptr;
+} ff_output_t;
 
-    while(true) {
-        pVCapture->read(frame);
-        imshow("Image", frame);
-        int key = waitKey(1);
-        if(key == 'q') break;
-    }
+typedef struct {
+    AVFormatContext *pFmtCtx    = nullptr;
+    AVCodecContext  *pCodecCtx  = nullptr;
+    AVInputFormat   *pInputFmt  = nullptr;
+    AVCodec         *pCodec     = nullptr;
+    AVStream        *pStream    = nullptr;
+    AVFrame         *pFrame     = nullptr;
+    AVPacket        *pPacket    = nullptr;
+} ff_input_t;
 
-    pVCapture->release();
-    delete(pVCapture);
-    destroyAllWindows();
+void Erro(string msg) {
+    clog << "Error: " << msg << endl;
+    exit(0);
 }
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    ReadingCamera();
+int main(int argc, char **argv) {
+
+    av_register_all();
+    avdevice_register_all();
+    avformat_network_init();
+
+    ff_input_t input;
+    //ff_output_t output;
+
+    //int videoId = -1;
+    int ret = 0;
+
+    input.pInputFmt = av_find_input_format("v4l2");
+    if(!input.pInputFmt) Erro("Input format could not be found");
+
+    input.pFmtCtx = avformat_alloc_context();
+    if(!input.pFmtCtx) Erro("Could not alloc input context");
+
+    ret = avformat_open_input(&input.pFmtCtx, "/dev/video0", input.pInputFmt, nullptr);
+    if(ret < 0) Erro("Could not open input");
+
+    AVCodecParameters *pCodecAux = nullptr;
+
+    for(unsigned int i = 0; i < input.pFmtCtx->nb_streams; ++i) {
+        pCodecAux = input.pFmtCtx->streams[i]->codecpar;
+        if(pCodecAux->codec_type == AVMEDIA_TYPE_VIDEO) {
+            input.pCodec = avcodec_find_decoder(pCodecAux->codec_id);
+            //videoId = i;
+            break;
+        }
+    }
+
+    input.pCodecCtx = avcodec_alloc_context3(input.pCodec);
+    if(!input.pCodecCtx) Erro("Could not alloc input codec context");
+
+    ret = avcodec_parameters_to_context(input.pCodecCtx, pCodecAux);
+    if(ret < 0) Erro("Could not copy parameters to context");
+
+    ret = avcodec_open2(input.pCodecCtx, input.pCodec, nullptr);
+    if(ret < 0) Erro("Could not open codec");
+
     return 0;
 }
